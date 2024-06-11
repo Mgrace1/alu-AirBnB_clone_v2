f45ccf79426ba8b0afc4aa1f5d8648fc75ca989d
Mgrace1 <g.munezero1@alustudent.com>
2024-06-11 13:48:40 +0000

    all tasks

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python3
-""" Script that starts a Flask application"""
+"""
+    python script that starts a Flask web application
+"""
+
 from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """ Function that displays "Hello HBNB" """
+@app.route('/')
+def hello_flask():
+    """
+        Return: string when route queried
+    """
     return 'Hello HBNB!'
+
+
 if __name__ == '__main__':
+    app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
